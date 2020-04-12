6c67d501553e62e25e7b2466103692cc6ea5fbfe
Mert Nacar <mertnacar2374@gmail.com>
2020-04-12 17:34:57 +0300

    Updates

---
+++ precache-manifest.6a61647423c0741cacb1e29c22297b5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a52005a0f9285f59902b923a3e516c7",
+    "url": "/create-synthetic-data/index.html"
+  },
+  {
+    "revision": "3223a6db226da5778559",
+    "url": "/create-synthetic-data/static/js/2.845ce871.chunk.js"
+  },
+  {
+    "revision": "02319550b42b2562b0f0cbc36f2d31db",
+    "url": "/create-synthetic-data/static/js/2.845ce871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1bae812d5a52a46c12",
+    "url": "/create-synthetic-data/static/js/main.7f2e0d1a.chunk.js"
+  },
+  {
+    "revision": "15cf2ce43f5d36aa3da7",
+    "url": "/create-synthetic-data/static/js/runtime-main.5117ad4a.js"
+  }
+]);
