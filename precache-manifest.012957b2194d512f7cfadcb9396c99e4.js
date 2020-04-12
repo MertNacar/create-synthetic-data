3b76dcd0fc9c9b9f95452a30fe0a2b79dca0b761
Mert Nacar <mertnacar2374@gmail.com>
2020-04-12 18:04:16 +0300

    Updates

--- precache-manifest.012957b2194d512f7cfadcb9396c99e4.js
+++ precache-manifest.012957b2194d512f7cfadcb9396c99e4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8c11165aa4cdf79275f6cb868dd476e",
+    "revision": "a11100fe57b9a0c80eb6c7eca6a18f16",
     "url": "/create-synthetic-data/index.html"
   },
   {
-    "revision": "d8dd57b56baa3e26ecda",
+    "revision": "cd431efe49579fdb0941",
     "url": "/create-synthetic-data/static/css/main.550f08ca.chunk.css"
   },
   {
-    "revision": "73bef00ea33621054529",
-    "url": "/create-synthetic-data/static/js/2.ba757d4b.chunk.js"
+    "revision": "d063a977b072d408af87",
+    "url": "/create-synthetic-data/static/js/2.3b124279.chunk.js"
   },
   {
     "revision": "02319550b42b2562b0f0cbc36f2d31db",
-    "url": "/create-synthetic-data/static/js/2.ba757d4b.chunk.js.LICENSE.txt"
+    "url": "/create-synthetic-data/static/js/2.3b124279.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d8dd57b56baa3e26ecda",
-    "url": "/create-synthetic-data/static/js/main.828fc347.chunk.js"
+    "revision": "cd431efe49579fdb0941",
+    "url": "/create-synthetic-data/static/js/main.0a2c0d1f.chunk.js"
   },
   {
     "revision": "15cf2ce43f5d36aa3da7",
